1220c8ce90566896551542aa34569b22570fb337
D024504 <heiko.theissen@sap.com>
2023-11-24 08:29:47 +0100

    Use express

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const express = require("express");
 const jimp = require("jimp");
 
-module.exports = async function(req, res) {
+module.exports = express()
+.get("/night-mode", async function(req, res) {
 	try {
-		var image = await jimp.read(new URL("s://" + req.url).searchParams.get("url"));
-		res.setHeader("Content-Type", image.getMIME());
+		var image = await jimp.read(req.query.url);
+		res.type(image.getMIME());
 		res.end(await image.invert().getBufferAsync(image.getMIME()));
 	} catch (e) {
-		res.statusCode = 400;
-		res.end(e.stack);
+		next(e);
 	}
-};
+});
